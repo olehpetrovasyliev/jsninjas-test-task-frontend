cdb6ce70b344ed94a951e8ad5a739285e1151f1f
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2024-10-31 15:33:20 +0200

    init + add types

---
+++ src/helpers/types.ts
@@ -0,0 +1,36 @@
+export interface PaginatedApiRes<T> {
+  page: number;
+  perPage: number;
+  totalPages: number;
+  results: T[];
+}
+
+export interface CharacterResData {
+  _id: string;
+  nickname: string;
+  real_name: string;
+  origin_description: string;
+  superpowers: string[];
+  catch_phrase: string;
+  images: string[];
+}
+
+export interface CharacterAddReqData {
+  _id: string;
+  nickname: string;
+  real_name: string;
+  origin_description: string;
+  superpowers: string[];
+  catch_phrase: string;
+  images: File[];
+}
+
+export interface CharacterEditReqData {
+  _id?: string;
+  nickname?: string;
+  real_name?: string;
+  origin_description?: string;
+  superpowers?: string[];
+  catch_phrase?: string;
+  images?: File[];
+}
